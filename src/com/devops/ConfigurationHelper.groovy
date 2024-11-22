fb1c3ba626abdf80bd50af5e81bc882abed7f834
levine824 <132388551+levine824@users.noreply.github.com>
2024-11-22 11:15:27 +0800

    fix: change lambda to foreach

--- src/com/devops/ConfigurationHelper.groovy
+++ src/com/devops/ConfigurationHelper.groovy
@@ -5,20 +5,30 @@ class ConfigurationHelper implements Serializable {
     static Properties removeQuotes(Properties props) {
         def propsWithoutQuotes = new Properties()
 
-        props.stringPropertyNames().forEach(key -> {
+        /* props.stringPropertyNames().forEach(key -> {
+             def valueWithoutQuotes = props.getProperty(key).replaceAll("^\"|\"\$", "")
+             propsWithoutQuotes.setProperty(key, valueWithoutQuotes)
+         })*/
+
+        for (String key : props.stringPropertyNames()) {
             def valueWithoutQuotes = props.getProperty(key).replaceAll("^\"|\"\$", "")
             propsWithoutQuotes.setProperty(key, valueWithoutQuotes)
-        })
+        }
 
         return propsWithoutQuotes
     }
 
     // Iterate properties and set variables "${key}=${value}"
     static setVariables(Script script, Properties props) {
-        props.stringPropertyNames().forEach(key -> {
+        /*props.stringPropertyNames().forEach(key -> {
             def value = props.getProperty(key)
             script "${key}=${value}"
-        })
+        })*/
+
+        for (String key : props.stringPropertyNames()) {
+            def value = props.getProperty(key)
+            script "${key}=${value}"
+        }
     }
 
 }
