1c8c6e331ad423e13ddd730963b720701277cc0c
levine824 <132388551+levine824@users.noreply.github.com>
2024-11-30 02:05:05 +0800

    fix: fail to judge exist

--- src/com/levine/ConfigurationLoader.groovy
+++ src/com/levine/ConfigurationLoader.groovy
@@ -1,14 +1,18 @@
 package com.levine
+
+//@Grab('org.yaml:snakeyaml:2.3')
+//import org.yaml.snakeyaml.Yaml
+
 @Grab('org.codehaus.groovy:groovy-yaml:3.0.23')
 import groovy.yaml.YamlSlurper
 import java.nio.file.Path
 import java.nio.file.Paths
 
 class ConfigurationLoader {
-    
+
     static def loadYaml(String path) {
         Path p = Paths.get(path)
-        if (!p.exists()) {
+        if (!Files.exists(p)) {
             throw new FileNotFoundException('Path not found!')
         }
         def config = new YamlSlurper().parse(p)
