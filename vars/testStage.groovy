5c6e8a6c402189ac31715d0698bf7c536b30f51c
levine824 <132388551+levine824@users.noreply.github.com>
2024-12-07 10:17:51 +0800

    fix: delete steps

--- vars/testStage.groovy
+++ vars/testStage.groovy
@@ -1,15 +1,18 @@
 @Grab('org.codehaus.groovy:groovy-astbuilder:3.0.23')
 import groovy.transform.Field
+import com.levine824.jenkins.configuration.*
 
 @Field String STAGE_NAME = getClass().getName()
 @Field String CONFIGURATION_TYPE = 'stage'
 
 def call(Map args = [:]) {
     stage(STAGE_NAME) {
+        def str = ConfigurationLoader.forTest()
         agent "${args.test}"
         
         script{
            echo "stageName:${STAGE_NAME}"
+           echo "str:${str}"
         }
 
     }
